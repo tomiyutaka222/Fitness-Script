c38551aadbc9fe49b4eaa1b39a2fb42265056ce4
tomiyukata222 <lettomiyutaka@au.com>
2021-11-05 00:09:34 +0900

    wp-config.phpに権限を追加
    FTP権限を付与する為にコードを追加

--- app/wp-config.php
+++ app/wp-config.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The base configuration for WordPress
  *
@@ -20,22 +21,25 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress');
+define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define( 'DB_USER', 'wordpress');
+define('DB_USER', 'wordpress');
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'wordpress');
+define('DB_PASSWORD', 'wordpress');
 
 /** MySQL hostname */
-define( 'DB_HOST', 'db:3306');
+define('DB_HOST', 'db:3306');
 
 /** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8');
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '');
+define('DB_COLLATE', '');
+
+/** プラグインを導入できるようにする TODO 開発後に消す */
+define('FS_METHOD', 'direct');
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -46,14 +50,14 @@ define( 'DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         '264ef23fe77926efc53adf481babf35d0b6b36e1');
-define( 'SECURE_AUTH_KEY',  '8bca66e9d9727c21c2b4aeb9630ad009bc2e0225');
-define( 'LOGGED_IN_KEY',    'ec1a3f8a0adff8fa0b735c567dcf4bde3d55d9a6');
-define( 'NONCE_KEY',        '6872f5288372c8059de0f08a6202b10e44a670c4');
-define( 'AUTH_SALT',        '7e80a1dc28c36752134abf28a7ee3db37e7c89e2');
-define( 'SECURE_AUTH_SALT', '88d22eaba2fccc541308160d30d383907ab9e674');
-define( 'LOGGED_IN_SALT',   '3e203b6077a1e4a622cc66747e6bc3f2bdc61337');
-define( 'NONCE_SALT',       '2c8b8b766d845b0acc8dec6c6b3c9db2aa1b505d');
+define('AUTH_KEY',         '264ef23fe77926efc53adf481babf35d0b6b36e1');
+define('SECURE_AUTH_KEY',  '8bca66e9d9727c21c2b4aeb9630ad009bc2e0225');
+define('LOGGED_IN_KEY',    'ec1a3f8a0adff8fa0b735c567dcf4bde3d55d9a6');
+define('NONCE_KEY',        '6872f5288372c8059de0f08a6202b10e44a670c4');
+define('AUTH_SALT',        '7e80a1dc28c36752134abf28a7ee3db37e7c89e2');
+define('SECURE_AUTH_SALT', '88d22eaba2fccc541308160d30d383907ab9e674');
+define('LOGGED_IN_SALT',   '3e203b6077a1e4a622cc66747e6bc3f2bdc61337');
+define('NONCE_SALT',       '2c8b8b766d845b0acc8dec6c6b3c9db2aa1b505d');
 
 /**#@-*/
 
@@ -77,7 +81,7 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define('WP_DEBUG', false);
 
 // If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
 // see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
@@ -88,8 +92,8 @@ if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROT
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+if (!defined('ABSPATH')) {
+	define('ABSPATH', __DIR__ . '/');
 }
 
 /** Sets up WordPress vars and included files. */
